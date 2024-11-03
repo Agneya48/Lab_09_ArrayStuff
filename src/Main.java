deccbacdb009cce33162342586a42a3d034e7d7a
Agneya48 <josh.t.hampton@gmail.com>
2024-11-03 15:16:56 -0500

    Completed Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] dataPoints = new int[100];
+        Random rnd = new Random();
+
+        for(int x = 0; x < dataPoints.length; x++) {
+            dataPoints[x] = rnd.nextInt(100) + 1;
+        }
+
+        for(int x = 0; x < dataPoints.length; x++) {
+            System.out.print(dataPoints[x] + " ");
+        }
+
+        System.out.println("\ncontinues offscreen for a while -->");
+        System.out.println("\nThe array length is " + dataPoints.length);
+        System.out.println();
+
+        for(int x = 0; x < dataPoints.length; x++) {
+            System.out.print(dataPoints[x] + " | ");
+        }
+        System.out.println("\nContinues off screen for a while -->");
+
     }
 }
